175ad3a116a5c79751815a0d746152b2eb4700bf
Ethem Çılgın <ethemc606@gmail.com>
2025-07-25 15:50:48 +0300

    Update build.sh idk what im doin

--- build.sh
+++ build.sh
@@ -165,9 +165,10 @@ wait
 rm -rf temp/tmp.*
 if [ -z "$(ls -A1 "${BUILD_DIR}")" ]; then abort "All builds failed."; fi
 
-log "\nInstall [Microg](https://github.com/ReVanced/GmsCore/releases) for non-root YouTube and YT Music APKs"
-log "Use [zygisk-detach](https://github.com/j-hc/zygisk-detach) to detach root ReVanced YouTube and YT Music from Play Store"
-log "\n[revanced-magisk-module](https://github.com/j-hc/revanced-magisk-module)\n"
+log "\n📥 Install [MicroG](https://github.com/inotia00/VancedMicroG/releases)"
+log "\n🛡️ Use [Zygisk Detach](https://github.com/ReVanced-Mirror/zygisk-detach)"
+log "\n🔗 [Zenic RVCM Builder](https://github.com/snapzenic/zenic_rvcm_builder)"
+log "\n📝 Changelog:"
 log "$(cat "$TEMP_DIR"/*-rv/changelog.md)"
 
 SKIPPED=$(cat "$TEMP_DIR"/skipped 2>/dev/null || :)
